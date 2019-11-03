81552019f3e29af204b95c36301b927178137611
Ryan Chan <ryanchan.ime@gmail.com>
2019-11-03 23:11:53 +0800

    modify

--- Identification.py
+++ Identification.py
@@ -1,28 +1,19 @@
 from PIL import Image
 import sys
 '''
-获取图片
 '''
 def getImage(filename):
     fileName = filename
     img = Image.open(fileName,'r')
-    # 打印当前图片的模式以及格式
-    #print('未转化前的: ', img.mode, img.format)
-    # 使用系统默认工具打开图片
-    #img.show()
     return img
 '''
-1) 将图片进行降噪处理, 通过二值化去掉后面的背景色并加深文字对比度
 '''
 def convert_Image(img, standard=250):
     '''
-    【灰度转换】
     '''
     image = img.convert('L')
 
     '''
-    【二值化】
-    根据阈值 standard , 将所有像素都置为 0(黑色) 或 255(白色), 便于接下来的分割
     '''
     pixels = image.load()
     for x in range(image.width):
@@ -36,16 +27,14 @@ def convert_Image(img, standard=250):
 import pytesseract
 import re
 '''
-使用 pytesseract 库来识别图片中的字符
+
 '''
 def change_Image_to_text(img):
     '''
-    如果出现找不到训练库的位置, 需要我们手动自动
-    语法: tessdata_dir_config = '--tessdata-dir "<replace_with_your_tessdata_dir_path>"'
+    
     '''
     testdata_dir_config = '--tessdata-dir "/usr/share/tesseract-ocr/4.00/tessdata"'
     textCode = pytesseract.image_to_string(img, lang='eng', config=testdata_dir_config)
-    # 去掉非法字符，只保留字母数字
     #print(textCode)
     textCode = re.sub("\W", "", textCode)
     return textCode
--- server.js
+++ server.js
@@ -8,29 +8,29 @@ const child_process = require('child_process');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/page"));
-app.listen("80",function () {
-    console.log("80端口服务启动：")
+app.listen("3000",function () {
+    console.log("3000端�~O��~\~M�~J��~P��~J���~Z")
 });
-//拦截请求
+//�~K��~H�请��~B
 app.post("/image",function (req,res) {
     var form = new formidable.IncomingForm();
     var returnData = "";
     //form.encoding = 'utf-8';
     form.uploadDir = path.join(__dirname + "/page/upload");
-    form.keepExtensions = true;//保留后缀
+    form.keepExtensions = true;//��~]�~U~Y�~P~N��~@
     form.maxFieldsSize = 2 * 1024 * 1024;
-    //处理图片
+    //��~D�~P~F�~[��~I~G
     form.parse(req, function (err, fields, files){
 
         var date = new Date();
         var time = date.getFullYear() + "_" + date.getMonth() + "_" + date.getDay() + "_" +
-        			 date.getHours() + "_" + date.getMinutes() + "_" + date.getMilliseconds();
+                                 date.getHours() + "_" + date.getMinutes() + "_" + date.getMilliseconds();
         var avatarName = time + '.png';
         fs.renameSync(files.upload.path, __dirname+"/page/upload/"+avatarName);
 
-		returnData = child_process.execSync('python Identification.py '+ __dirname+"/page/upload/"+avatarName);
-		//console.log(""+returnData);
-		res.send(""+returnData);
-	})
+                returnData = child_process.execSync('python3 Identification.py '+ __dirname+"/page/upload/"+avatarName);
+                //console.log(""+returnData);
+                res.send(""+returnData);
+        })
 
 })
